14395f3961fc7256f1792e796d9a164503102fd3
fauzanajipray <fauzan.aji@rectmedia.co.id>
2024-05-23 08:26:37 +0700

    initial firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,39 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:konsul/firebase_options.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  // WidgetsBinding widgetsBinding =
+  WidgetsFlutterBinding.ensureInitialized();
+  // Inisialisasi HydratedStorage
+  final storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getApplicationDocumentsDirectory(),
+  );
+  HydratedBloc.storage = storage;
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  // Inisialisasi Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Mengatur preferensi orientasi layar
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
+  // Menjalankan aplikasi
+  runApp(const App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
